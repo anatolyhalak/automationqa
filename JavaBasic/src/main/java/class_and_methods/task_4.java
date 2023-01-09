912433406fbc6d52f4e9b601410d20c687294c4f
Anatoly Khalak <inno0747@gmail.com>
2023-01-09 20:02:17 +0300

    Практика Java. Есть ли позитив.

---
+++ JavaBasic/src/main/java/class_and_methods/task_4.java
@@ -0,0 +1,27 @@
+package JavaBasic.src.main.java.class_and_methods;
+
+public class task_4 {
+    public static void main(String[] args) {
+
+        System.out.println(isPositive(1));
+    }
+
+    public static boolean isPositive(int x) {
+
+        return x >= 1;
+
+    }
+}
+
+/*
+Классы и методы #4
+Есть ли позитив. Дана следующая сигнатура метода:
+public static boolean isPositive(int x);
+Необходимо реализовать метод таким образом, чтобы он принимал число x и возвращал true если оно положительное.
+Пример 1:
+x=3
+результат: true
+Пример 2:
+x=-5
+результат: false
+ */
